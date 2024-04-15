b0b4618cee452a88dbcf00b1dda6aa613b1539e7
Mohammed Adnan K <61493304+mdadnan123@users.noreply.github.com>
2024-04-15 17:10:17 +0530

    Update settings.py
    Removed the secret key from code in repo.

--- video_chat_app/settings.py
+++ video_chat_app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lwa0wh9e3q4hskmo(3qzax-@v5&vkfrdo5$nr79ikb#h-#u-*h'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
